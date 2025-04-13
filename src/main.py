2b36a8f6d5718f633ee945a85bd19c4ac13792c4
t6o_o6t <51913600+coolwind0202@users.noreply.github.com>
2025-04-13 14:42:29 +0000

    Fix PDF parse algorithm

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ import requests
 import pymupdf
 import more_itertools
 
-logging.basicConfig(filename="debug.log", encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename="debug.log", encoding="utf-8", level=logging.DEBUG)
 
 DOMAIN = "https://www.chitose.ac.jp"
 SYLLABUSES_PAGE_PATH = "/info/info_index/311"
@@ -27,14 +27,23 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(resp.text, "html.parser")
 
-    syllabus_paths = [a["href"] for a in soup.find_all("a") if "シラバス" in str(a)]
+    syllabus_paths = [
+        a["href"]
+        for a in soup.find_all("a")
+        if "シラバス" in str(a) and "大学院" not in str(a)
+    ]
+
     syllabus_urls = [urljoin(DOMAIN, path) for path in syllabus_paths]
 
     syllabus_resps = (requests.get(url) for url in syllabus_urls)
-    syllabus_contents = (resp.content for resp in syllabus_resps if resp.status_code == requests.codes.ok)
-    
+    syllabus_contents = (
+        resp.content for resp in syllabus_resps if resp.status_code == requests.codes.ok
+    )
+
     docs = (pymupdf.Document(stream=content) for content in syllabus_contents)
-    subjects = models.Subjects(subjects=more_itertools.flatten(pdf.parse_pdf(doc) for doc in docs))
+
+    s = list(more_itertools.flatten(pdf.parse_pdf(doc) for doc in docs))
+    subjects = models.Subjects(subjects=s)
 
     dump = subjects.model_dump_json()
     with open("out.json", "w") as f:
--- src/parser/pdf.py
+++ src/parser/pdf.py
@@ -6,107 +6,122 @@ import re
 import pymupdf
 import more_itertools
 
-def excludes(l, targets):
-  return (x for x in l if x not in targets)
 
-def parse_point(s):
+def excludes(l, targets):
+    return (x for x in l if x not in targets)
+
+
+def str_to_int(s):
     match = re.match(r"\d+", s.strip())
     if match is None:
-       return 0
-    
+        return 0
+
     return int(match.group(0))
 
+
 def parse_chunk(chunk) -> models.Subject:
-  name, *_ = excludes(chunk[0], ["科 目 名", None])
-  logging.debug("Parse %s", name)
+    _, name, *_ = excludes(chunk[0], [None])
+    logging.debug("Parse %s", name)
 
-  raw_school_year, required_elective, raw_is_CAP_target = excludes(chunk[1], ["配 当 学 年", "必修・選択", "ＣＡＰ制", None])
+    _, raw_school_year, _, required_elective, _, raw_is_CAP_target, *_ = excludes(
+        chunk[1], [None]
+    )
 
-  school_year = int(raw_school_year[0])
-  is_required = "必修" in required_elective
-  is_elective = "選択" in required_elective
-  is_CAP_target = raw_is_CAP_target == "必修"
+    school_year = str_to_int(raw_school_year)
+    is_required = "必修" in required_elective
+    is_elective = "選択" in required_elective
+    is_CAP_target = raw_is_CAP_target == "必修"
 
-  logging.debug(school_year)
-  logging.debug(is_required)
-  logging.debug(is_elective)
-  logging.debug(is_CAP_target)
+    logging.debug(school_year)
+    logging.debug(is_required)
+    logging.debug(is_elective)
+    logging.debug(is_CAP_target)
 
-  category, raw_credits, _ = excludes(chunk[2], ["授 業 の 種 類", "単位数", "授業回数", None])
-  credits = int(raw_credits[0])
+    _, category, _, raw_credits, _, raw_cource_count, *_ = excludes(chunk[2], [None])
+    credits = str_to_int(raw_credits)
+    cource_count = str_to_int(raw_cource_count)
 
-  logging.debug(category)
-  logging.debug(credits)
+    logging.debug(category)
+    logging.debug(credits)
+    logging.debug(cource_count)
 
+    _, raw_teacher_names, _, credit_manager, *_ = excludes(chunk[3], [None])
+    teacher_names = "".join(raw_teacher_names).split("、")
+    logging.debug(teacher_names)
+    logging.debug(credit_manager)
 
-  raw_teacher_names, credit_manager = excludes(chunk[3], ["授 業 担 当 者", "単位認定責任者", None])
-  teacher_names = "".join(raw_teacher_names).split("、")
-  logging.debug(teacher_names)
-  logging.debug(credit_manager)
+    _, summary, *_ = excludes(chunk[6], [None])
+    logging.debug(summary)
 
+    _, goal, *_ = excludes(chunk[7], [None])
+    logging.debug(goal)
 
-  summary, *_ = excludes(chunk[6], ["授業科目の概要", None])
-  logging.debug(summary)
+    raw_evaluation_items = (list(excludes(row, [None, ""])) for row in chunk[9:18])
+    evaluation_items = (
+        models.EvaluationItem(title=title, point=str_to_int(detail[0]), way=detail[1])
+        for title, *detail in raw_evaluation_items
+        if len(detail) == 2
+    )
+    logging.debug(evaluation_items)
 
+    cources = more_itertools.collapse(excludes(row[1:], [None]) for row in chunk[19:34])
+    logging.debug(cources)
 
-  goal, *_ = excludes(chunk[7], ["授 業 科 目 の\n到 達 目 標", None])
-  logging.debug(goal)
+    _, self_study, *_ = excludes(chunk[34], [None])
+    logging.debug(self_study)
 
-  raw_evaluation_items = (list(excludes(row, [None, ""])) for row in chunk[9: 18])
-  evaluation_items = (
-      models.EvaluationItem(title=title, point=parse_point(detail[0]), way=detail[1])
-      for title, *detail in raw_evaluation_items if len(detail) == 2
-  )
-  logging.debug(evaluation_items)
+    _, textbook, *_ = excludes(chunk[35], [None])
+    logging.debug(textbook)
 
-  cources = (more_itertools.collapse(excludes(row[1:], [None]) for row in chunk[19:34]))
-  logging.debug(cources)
+    _, reference, *_ = excludes(chunk[36], [None])
+    logging.debug(reference)
 
-  self_study, *_ = excludes(chunk[34], ["授業外学修について", None])
-  logging.debug(self_study)
+    _, *raw_evaluation_events = excludes(list(zip(*chunk[37:40])), [(None, None, None)])
+    evaluation_events = [
+        models.EvaluationEvent(title=title, point=str_to_int(raw_point))
+        for title, _, raw_point in raw_evaluation_events
+    ]
+    logging.debug(evaluation_events)
 
-  textbook, *_ = excludes(chunk[35], ["教 科 書", None])
-  logging.debug(textbook)
+    _, evaluation_criterion, *_ = excludes(chunk[40], [None])
+    logging.debug(evaluation_criterion)
 
-  reference, *_ = excludes(chunk[36], ["参 考 文 献", None])
-  logging.debug(reference)
+    _, additional_information, *_ = excludes(chunk[41], [None])
+    logging.debug(additional_information)
 
-  raw_evaluation_events = (excludes(list(zip(*chunk[37:40])), [(None, None, None), ("試 験 等 の 実 施", None, "成績評価の割合")]))
-  evaluation_events = [
-      models.EvaluationEvent(title=title, point=parse_point(raw_point))
-      for title, _, raw_point in raw_evaluation_events
-  ]
-  logging.debug(evaluation_events)
+    return models.Subject(
+        name=name,
+        school_year=school_year,
+        is_required=is_required,
+        is_elective=is_elective,
+        is_CAP_target=is_CAP_target,
+        category=category,
+        credits=credits,
+        teacher_names=teacher_names,
+        credit_manager=credit_manager,
+        summary=summary,
+        goal=goal,
+        evaluation_items=evaluation_items,
+        cources=cources,
+        self_study=self_study,
+        textbook=textbook,
+        reference=reference,
+        evaluation_events=evaluation_events,
+        evaluation_criterion=evaluation_criterion,
+        additional_information=additional_information,
+    )
 
-  evaluation_criterion, *_ = excludes(chunk[40], ["成績評価の基準", None])
-  logging.debug(evaluation_criterion)
-  additional_information, *_ = excludes(chunk[41], ["試験等の実施、成\n績評価の基準に関\nする補足事項", None])
-  logging.debug(additional_information)
-
-  return models.Subject(
-      name=name,
-      school_year=school_year,
-      is_required=is_required,
-      is_elective=is_elective,
-      is_CAP_target=is_CAP_target,
-      category= category,
-      credits= credits,
-      teacher_names=teacher_names,
-      credit_manager=credit_manager,
-      summary=summary,
-      goal=goal,
-      evaluation_items=evaluation_items,
-      cources=cources,
-      self_study=self_study,
-      textbook=textbook,
-      reference=reference,
-      evaluation_events=evaluation_events,
-      evaluation_criterion=evaluation_criterion,
-      additional_information=additional_information
-  )
 
 def parse_pdf(doc: pymupdf.Document) -> list[models.Subject]:
-    nested_rows = ([table.extract() for table in page.find_tables().tables] for page in doc)
+    nested_rows = (
+        (
+            table.extract()
+            for table in page.find_tables(
+                clip=pymupdf.Rect(0, page.rect.height * 0.06, page.rect.br)
+            ).tables
+        )
+        for page in doc
+    )
     rows = list(more_itertools.collapse(nested_rows, levels=2))
 
     chunks = list(more_itertools.chunked(rows, 42))
